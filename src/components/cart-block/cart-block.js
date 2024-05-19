5d50a3e76ab75e847ea365407637b239f34a5fac
HoloborodkoM <mykhailoholoborodko@gmail.com>
2024-05-19 18:25:38 +0300

    add icon and styles for component 'CartBlock'

--- src/components/cart-block/cart-block.js
+++ src/components/cart-block/cart-block.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './cart-block.css';
+import { IoCartOutline } from "react-icons/io5";
 
 export const CartBlock = () => {
    return (
       <div className="cart-block">
-         CartBlock
+         <IoCartOutline size={25} className="cart-block-icon"/>
+         <span className="cart-block-total-price">233$</span>
       </div>
    )
 } 
