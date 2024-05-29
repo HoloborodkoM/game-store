9175893e2af343bbcb59758b9be58fa6ffad4f6c
HoloborodkoM <mykhailoholoborodko@gmail.com>
2024-05-29 13:21:51 +0300

    add component 'CartMenu'

--- src/components/cart-block/cart-block.js
+++ src/components/cart-block/cart-block.js
@@ -3,15 +3,17 @@ import './cart-block.css';
 import { IoCartOutline } from 'react-icons/io5';
 import { useSelector } from 'react-redux';
 import { calcTotalPrice } from '../utils';
+import { CartMenu } from '../cart-menu';
 
 export const CartBlock = () => {
    const items = useSelector((state) => state.cart.itemsInCart);
    const totalPrice = calcTotalPrice(items);
-   
+
    return (
       <div className="cart-block">
          <IoCartOutline size={25} className="cart-block-icon"/>
          <span className="cart-block-total-price">{ totalPrice }$</span>
+         <CartMenu items={items} onClick={() => null}/>
       </div>
    )
 }
---
+++ src/components/cart-menu/cart-menu.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './cart-menu.css';
+import { calcTotalPrice } from '../utils';
+import { Button } from '../button';
+
+export const CartMenu = ({ items, onClick }) => {
+   return (
+      <div className="cart-menu">
+         <div className="cart-menu-games-list">
+            {
+               items.length > 0 ? items.map(game => game.title) : 'Empty cart'
+            }
+         </div>
+         {
+            items.length > 0 ?
+               <div className="cart-menu-arrange">
+                  <div className="cart-menu-total-price">
+                     <span>Total price: </span>
+                     <span>{calcTotalPrice(items)}$</span>
+                  </div>
+                  <Button type="primary" size="m" onClick={onClick}>
+                     Order
+                  </Button>
+               </div>
+            : null
+         }
+      </div>
+   );
+};
