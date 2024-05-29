e640f6e64e615665d1731cdf94009f22d6d57a02
HoloborodkoM <mykhailoholoborodko@gmail.com>
2024-05-29 19:18:02 +0300

    implement order page and add component 'OrderItem'

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -1,1 +1,5 @@
 export const calcTotalPrice = (items) => items.reduce((acc, item) => (acc += item.price), 0);
+
+export const enumerate = (num, dec) => {
+   return num === 1 ? dec[0] : dec[1];
+};
--- src/pages/order-page/order-page.jsx
+++ src/pages/order-page/order-page.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useSelector} from 'react-redux';
+import { OrderItem } from '../../components/order-item';
+import { calcTotalPrice, enumerate } from '../../components/utils';
 import './order-page.css';
 
 export const OrderPage = () => {
+   const items = useSelector((state) => state.cart.itemsInCart);
+
+   if (items.length < 1) {
+      return <h1>Your cart is empty</h1>
+   }
+
    return (
       <div className="order-page">
-         Order Page
+         <div className="order-page-left">
+            { items.map(game => <OrderItem game={game}/>) }
+         </div>
+         <div className="order-page-right">
+            <div className="order-page-total-price">
+               <span>{ items.length } 
+                  { enumerate(
+                     items.length, 
+                     ['game', 'games']
+                  )} at the rate of { calcTotalPrice(items) }$
+               </span>
+            </div>
+         </div>
       </div>
    );
 };
