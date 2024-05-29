6f8c6b615b10ef3a47431ced3396460068eeb20b
HoloborodkoM <mykhailoholoborodko@gmail.com>
2024-05-29 13:44:47 +0300

    implement functional of delete game from cart

--- src/components/game-buy/game-buy.js
+++ src/components/game-buy/game-buy.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import './game-buy.css';
 import { Button } from '../button';
-import { useDispatch } from 'react-redux';
-import { setItemInCart } from '../../store/cart/reducer';
+import { useDispatch, useSelector } from 'react-redux';
+import { setItemInCart, deleteItemFromCart } from '../../store/cart/reducer';
 
 export const GameBuy = ({ game }) => {
    const dispatch = useDispatch();
+   const items = useSelector((state) => state.cart.itemsInCart);
+   const isItemInCart = items.some((item) => item.id === game.id);
 
    const handleClick = (event) => {
       event.stopPropagation();
-      dispatch(setItemInCart(game));   
-   };
+      
+      if (isItemInCart) {
+        dispatch(deleteItemFromCart(game.id));
+      } else {
+        dispatch(setItemInCart(game));
+      }
+    };
 
    return (
       <div className="game-buy">
          <span className="game-buy-price">{game.price} $</span>
          <Button
-            type="primary"
+            type={isItemInCart ? "secondary" : "primary" }
             onClick={handleClick}
          >
-            Add to cart
+            {isItemInCart ? 'Delete from cart' : 'Add to cart'}
          </Button>
       </div>
    )
-} 
+}
