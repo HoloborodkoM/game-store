f72ecd8feffaeb575812237ef1ad57aae88ec743
HoloborodkoM <mykhailoholoborodko@gmail.com>
2024-05-29 12:38:02 +0300

    implement functional of add prices to cart

--- src/components/cart-block/cart-block.js
+++ src/components/cart-block/cart-block.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './cart-block.css';
-import { IoCartOutline } from "react-icons/io5";
+import { IoCartOutline } from 'react-icons/io5';
+import { useSelector } from 'react-redux';
+import { calcTotalPrice } from '../utils';
 
 export const CartBlock = () => {
+   const items = useSelector((state) => state.cart.itemsInCart);
+   const totalPrice = calcTotalPrice(items);
+   
    return (
       <div className="cart-block">
          <IoCartOutline size={25} className="cart-block-icon"/>
-         <span className="cart-block-total-price">233$</span>
+         <span className="cart-block-total-price">{ totalPrice }$</span>
       </div>
    )
-} 
+}
--- src/components/game-buy/game-buy.js
+++ src/components/game-buy/game-buy.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './game-buy.css';
 import { Button } from '../button';
+import { useDispatch } from 'react-redux';
+import { setItemInCart } from '../../store/cart/reducer';
 
 export const GameBuy = ({ game }) => {
+   const dispatch = useDispatch();
+
+   const handleClick = (event) => {
+      event.stopPropagation();
+      dispatch(setItemInCart(game));   
+   };
+
    return (
       <div className="game-buy">
          <span className="game-buy-price">{game.price} $</span>
          <Button
             type="primary"
-            onClick={ () => null }
+            onClick={handleClick}
          >
             Add to cart
          </Button>
